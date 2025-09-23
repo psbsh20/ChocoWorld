f07a2ef9bd56fa559bf383efcf635978efe8a1bc
Park Sung-ho <psbsh20@pickn.kr>
2025-09-23 18:58:19 +0900

    prettier 적용

---
+++ packages/prettier-config/base.cjs
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  printWidth: 120,
+  tabWidth: 2,
+  arrowParens: "always",
+  endOfLine: "lf",
+};
