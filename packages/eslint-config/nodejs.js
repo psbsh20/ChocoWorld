c7db57119bfddb39056ef3419639ff8abc742f11
Park Sung-ho <psbsh20@pickn.kr>
2025-09-23 11:46:50 +0900

    server-nodejs init

---
+++ packages/eslint-config/nodejs.js
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import pluginReact from "eslint-plugin-react";
+import tseslint from "typescript-eslint";
+import { config as baseConfig } from "./base.js";
+
+/**
+ * A custom ESLint configuration for libraries that use React.
+ *
+ * @type {import("eslint").Linter.Config[]} */
+export const config = [
+  ...baseConfig,
+  js.configs.recommended,
+  eslintConfigPrettier,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
